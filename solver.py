a165c908f6220f60c7589eacdea2af7d5967edd2
thisIsQuy <condragoon@gmail.com>
2024-01-10 06:03:55 -0600

    formatted grid into a list of rows; too much hassel using dictionary; iterated inputs into the grid

--- solver.py
+++ solver.py
@@ -5,28 +5,34 @@
 # modify string using ascii codes to replace 46 (.) with 48 (0), then typecast it into a list of characters
 inputs = list("......43...52..8.7.8.7......3..48.5..4.6....2.9..52.4..6.4.......19..6.3......57.".translate({46: 48}))
 
-# convert elements of var "inputs" into List of in
+# convert elements of var "inputs" into list of ints
 inputs = [eval(i) for i in inputs]
 
 # Rows notation: rowTopTop (rtt), rowTopCenter (rtc),
 # rowTopBottom (rtb), rowMiddleBottom (rmb), and rowBottomBottom (rbb)
-grid = {
-  "rtt": [0,0,0,0,0,0,0,0,0],
-  "rtc": [0,0,0,0,0,0,0,0,0],
-  "rtb": [0,0,0,0,0,0,0,0,0],
-  "rmt": [0,0,0,0,0,0,0,0,0],
-  "rmc": [0,0,0,0,0,0,0,0,0],
-  "rmb": [0,0,0,0,0,0,0,0,0],
-  "rbt": [0,0,0,0,0,0,0,0,0],
-  "rbc": [0,0,0,0,0,0,0,0,0],
-  "rbb": [0,0,0,0,0,0,0,0,0]
-}
+grid = [
+         [0,0,0,0,0,0,0,0,0],   # rtt
+         [1,2,3,4,5,6,7,8,9],   # rtc
+         [0,0,0,0,0,0,0,0,0],   # rtb
+         [0,0,0,0,0,0,0,0,0],   # rmt
+         [0,0,0,0,0,0,0,0,0],   # rmc
+         [0,0,0,0,0,0,0,0,0],   # rmb
+         [0,0,0,0,0,0,0,0,0],   # rbt
+         [0,0,0,0,0,0,0,0,0],   # rbc
+         [0,0,0,0,0,0,0,0,0]    # rbb
+]
 
 # updating initial sudoku state onto grid
 n = 0
-for k, l in grid.items():
-  grid[k] = inputs[n:n+len(l)]
-  n += len(l)
+for i in range(len(grid)):
+  for j in range(len(grid[i])):
+    grid[i][j] = inputs[n]
+    n += 1
+
+
+
+
+
 
 print(grid)
 
